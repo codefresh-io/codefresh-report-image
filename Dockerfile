a3971ccc01c29f707533dd18ca0581009e00e4fd
saffi <saffi.hartal@codefresh.io>
2022-05-08 17:29:44 +0300

    wip

--- Dockerfile
+++ Dockerfile
@@ -1,46 +1,24 @@
 FROM node:16.14.2-alpine as base
 WORKDIR /code
-COPY lerna.json .
 COPY tsconfig.json .
-COPY xxx/package.json .
+COPY tsconfig.build.json .
+COPY package.json .
 COPY yarn.lock .
-
-FROM base as libs
-COPY --from=base /code /code
-
-COPY libs libs
-# TODO: Remove libs not used by the app
-# RUN yarn workspaces info | tail -n +2 | tail -r | tail -n +2 | tail -r | jq ".\"$APP\".workspaceDependencies | .[]" | sed 's@\@codefresh-io/@@' | xargs rm -rf
-
-FROM base as libs-packages-only
-COPY --from=libs /code/libs /code/libs
-
-# Leave only package.json files
-RUN find libs -not -name "package.json" -mindepth 2 -maxdepth 2 | xargs rm -rf
+# Install dependencies
+RUN yarn --cwd /code install --frozen-lockfile
 
 FROM base as build
-COPY --from=base /code /code
-
-# Install dependencies
-ARG APP
-COPY --from=libs-packages-only /code/libs /code/libs
-COPY apps/${APP}/package.json apps/${APP}/package.json
-RUN yarn --cwd apps/${APP} install --frozen-lockfile
-
+# add SRC
+COPY src /code/src
+RUN ls -ltr /code/src
 # Build typescript
-COPY --from=libs /code/libs /code/libs
-COPY apps/${APP} apps/${APP}
-RUN yarn build
+RUN yarn --cwd /code build
 
 # Clean up dev dependencies
 RUN yarn install --production
 
 FROM base
-ARG APP
-
-COPY --from=build /code/libs /code/libs
 COPY --from=build /code/node_modules /code/node_modules
-COPY --from=build /code/apps/${APP} /code/apps/${APP}
-
-WORKDIR /code/apps/${APP}
+COPY --from=build /code/src /code/src
+WORKDIR /code
 CMD [ "yarn", "start" ]
--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,9 @@ async function main(): Promise<void> {
     if (!payload['CF_API_KEY']) {
         console.error(`CF_API_KEY must be provided as environment variable.`)
     }
+    if (!payload['IMAGE']) {
+        console.error(`IMAGE must be provided as environment variable.`)
+    }
     const cfHost = payload['CF_HOST'] || 'g.codefresh.io'
     const esc = encodeURIComponent;
     const qs = Object.entries(payload).map(kv => `${esc(kv[0])}=${esc(kv[1]||'')}`).join('&')
@@ -30,31 +33,27 @@ async function main(): Promise<void> {
         console.debug(`Payload: ${JSON.stringify(payload, null, 2)}`)
         console.debug(`Sending request: ${url}`)
     }
-    const eventSource = new EventSource(url)
-    eventSource.addEventListener('report', function (event) {
-        console.info(`report =>`, JSON.stringify(JSON.parse(event.data), null, 2))
-    })
-    eventSource.addEventListener('info', function (event) {
-        console.info(JSON.stringify(event.data))
-    })
-    eventSource.addEventListener('warn', function (event) {
-        console.warn(JSON.stringify(event.data))
-    })
-    eventSource.addEventListener('error', function (event) {
-        console.error(JSON.stringify(event.data))
-    })
-    eventSource.addEventListener('end', function (event) {
-        console.log('Done')
-    })
-
     const waitFor = new Promise((resolve, reject) => {
+
+        const eventSource = new EventSource(url)
+        eventSource.addEventListener('report', function (event) {
+            console.info(`report =>`, JSON.stringify(JSON.parse(event.data), null, 2))
+        })
+        eventSource.addEventListener('info', function (event) {
+            console.info(`\t\t${JSON.stringify(event.data)}`)
+        })
+        eventSource.addEventListener('warn', function (event) {
+            console.warn(`Warning:\t${JSON.stringify(event.data)}`)
+        })
+        eventSource.addEventListener('error', (err) => {
+            const {error, parameters} = JSON.parse(err.data)
+            console.error(`Error:\t${error.name}: ${error.message}, parameters provided: ${Object.keys(parameters)}`)
+            eventSource.close()
+            reject(error)
+        })
         eventSource.addEventListener('end', (ev) => {
             eventSource.close()
             resolve(ev)
-        })
-        eventSource.addEventListener('error', (error) => {
-            eventSource.close()
-            reject(error)
         })
     })
     await waitFor
