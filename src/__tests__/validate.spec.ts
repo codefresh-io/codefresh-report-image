a46ea22f71a6594ea9a35e723c9856fe37d1c25a
saffi <saffi.hartal@codefresh.io>
2022-05-25 18:10:45 +0300

    fix test.

--- src/__tests__/validate.spec.ts
+++ src/__tests__/validate.spec.ts
@@ -6,7 +6,9 @@ describe('client report-image validation', () => {
         try {
             validate({ ENV1: '1', someVal: 'ignored' })
         } catch (error) {
-            expect(error.message).toBe(`Validation Error: ["CF_API_KEY must be provided as environment variable.","CF_IMAGE must be provided as environment variable."]`)
+            expect(error.message).toBe(`Validation Error: ["CF_API_KEY must be provided as environment variable.",` +
+                `"CF_IMAGE must be provided as environment variable.",`+
+                `"CF_HOST must be provided as app-proxy http/s address"]`)
             return
         }
         fail(`should have thrown Validation Error`)
@@ -21,19 +23,6 @@ describe('client report-image validation', () => {
         expect(res).toEqual({
             'CF_API_KEY': '1',
             'CF_HOST': 'host.io',
-            'CF_IMAGE': 'testImage'
-        })
-    })
-    it('default CF_HOST', async () => {
-        let res
-        try {
-            res = validate({ CF_API_KEY: '1', alsoIgnored: 'ignored', IGNORED: 'ignored too', CF_IMAGE: 'testImage' })
-        } catch (error) {
-            fail(`should have not thrown an error ${JSON.stringify(error)}`)
-        }
-        expect(res).toEqual({
-            'CF_API_KEY': '1',
-            'CF_HOST': 'g.codefresh.io',
             'CF_IMAGE': 'testImage'
         })
     })
