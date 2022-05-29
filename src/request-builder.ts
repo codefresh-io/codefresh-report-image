e2a6207363c428d811e9a9a135413d539c9f9d24
saffi <saffi.hartal@codefresh.io>
2022-05-29 15:54:13 +0300

    keep the CF_ prefix

--- src/request-builder.ts
+++ src/request-builder.ts
@@ -1,29 +1,14 @@
 /**
- * remove codefresh vars prefix
- * @param env
- */
-function cleanThePrefixCF(env: Record<string, string|undefined>): Record<string, string|undefined> {
-    return Object.keys(env)
-        .reduce((obj, key) => {
-            const k = key.replace(/^CF_/, '')
-            obj[k] = env[key]
-            return obj
-        }, {})
-}
-
-/**
  * Build image-report url and headers
  * @param payload
- * @param esc
  */
 export function buildUrlHeaders(payload: Record<string, string | undefined>) {
     const esc = encodeURIComponent
-    const parameters = cleanThePrefixCF(payload)
-    const headers = { 'authorization': parameters['API_KEY'] }
-    const host = parameters['HOST']
-    delete parameters['API_KEY']
-    delete parameters['HOST']
-    const qs = Object.entries(parameters).map(kv => `${esc(kv[0])}=${esc(kv[1] || '')}`).join('&')
+    const headers = { 'authorization': payload['CF_API_KEY'] }
+    const host = payload['CF_HOST']
+    delete payload['CF_API_KEY']
+    delete payload['CF_HOST']
+    const qs = Object.entries(payload).map(kv => `${esc(kv[0])}=${esc(kv[1] || '')}`).join('&')
     const url = `${host}/api/image-report?${qs}`
     return { url, headers }
 }
