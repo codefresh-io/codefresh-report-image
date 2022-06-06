b25a22018157d12441502df5782a535c5bbd6417
vadimgusev-codefresh <vadim.gusev@codefresh.io>
2022-06-06 18:07:52 +0300

    CR11315 (#27)
    * keep the CF_ prefix
    
    Co-authored-by: saffi <saffi.hartal@codefresh.io>

--- src/__tests__/request-builder.spec.ts
+++ src/__tests__/request-builder.spec.ts
@@ -2,14 +2,14 @@ import { buildUrlHeaders } from '../request-builder'
 
 
 describe('request builder test', () => {
-    it('clean CF_', async () => {
+    it('not clean CF_', async () => {
         const
             { url, headers } = buildUrlHeaders({
                 'CF_API_KEY': 'the-token',
                 'CF_HOST': 'g.codefresh.io',
                 'CF_IMAGE': 'testImage'
             })
-        expect(url).toEqual('g.codefresh.io/api/image-report?IMAGE=testImage')
+        expect(url).toEqual('g.codefresh.io/api/image-report?CF_IMAGE=testImage')
         expect(headers).toEqual({ 'authorization': 'the-token' })
     })
 
