4e00378d82b3735e6b7a4f8f914e722fd5a578cb
eti-codefresh <eti.zaguri@codefresh.io>
2022-07-27 13:59:33 +0300

    TEST

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import EventSource from 'eventsource'
 
-// import { GraphQLClient, gql, ClientError } from 'graphql-request'
 import { validate } from './validate'
 import { tryParseJson, errors, buildUrlHeaders } from './utils'
 import { logger, workflowLogger } from './logger'
@@ -15,10 +14,8 @@ async function main(argv, env): Promise<void> {
     if (verbose) {
         logger.debug('running with verbose log')
     }
-
     const payload = validate(env)
-
-    const { url, headers } = buildUrlHeaders(payload)
+    const { url, headers } = await buildUrlHeaders(payload)
     if (verbose) {
         logger.debug(`payload: ${JSON.stringify(payload, null, 2)}`)
         logger.debug(`sending request: ${url}, headers: ${JSON.stringify(headers)}`)
