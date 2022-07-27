65fd000b5400ea4ef97f5b55236b574f58aacc9e
eti-codefresh <eti.zaguri@codefresh.io>
2022-07-27 15:45:53 +0300

    wip

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { GraphQLClient, gql } from 'graphql-request'
+import { get } from 'lodash'
 export const tryParseJson = (str: string) => {
     try {
         return JSON.parse(str)
@@ -15,27 +16,14 @@ export const tryParseJson = (str: string) => {
  export async function buildUrlHeaders(payload: Record<string, string | undefined>) {
     const esc = encodeURIComponent
     const headers = { 'authorization': payload['CF_API_KEY']! }
-    const runtimeName = payload['CF_RUNTIME_NAME'] as string
+    const runtimeName = payload['CF_RUNTIME_NAME']
     let host
     if (!runtimeName) {
-        host = payload['CF_HOST'] as string
+        host = payload['CF_HOST']
         delete payload['CF_HOST']
     }
     else {
-        const platformHost = payload['CF_HOST_URL'] || 'https://g.codefresh.io'
-        const graphQLClient = new GraphQLClient(`${platformHost}/2.0/api/graphql`, {
-            headers
-        })
-
-        const getRuntimeIngressHostQuery = gql`
-            query Runtime($name: String!) {
-                runtime(name: $name) {
-                    ingressHost
-                }
-            }`
-
-        const res = await graphQLClient.request(getRuntimeIngressHostQuery, { name: runtimeName })
-        host = res.runtime.ingressHost as string
+        host = getRuntimeIngressHost(runtimeName, headers)
         delete payload['CF_RUNTIME_NAME']
     }
     delete payload['CF_API_KEY']
@@ -47,6 +35,27 @@ export const tryParseJson = (str: string) => {
     return { url, headers }
 }
 
+
+async function getRuntimeIngressHost(runtimeName: string, headers: Record<string, string>, platformHost = 'https://g.codefresh.io'): Promise<string> {
+    const graphQLClient = new GraphQLClient(`${platformHost}/2.0/api/graphql`, {
+        headers
+    })
+
+    const getRuntimeIngressHostQuery = gql`
+        query Runtime($name: String!) {
+            runtime(name: $name) {
+                ingressHost
+            }
+        }`
+
+    const res = await graphQLClient.request(getRuntimeIngressHostQuery, { name: runtimeName })
+    const ingressHost = get(res, 'runtime.ingressHost')
+    if (!ingressHost) {
+        const message = res.runtime ? `ingress host is not defined on your '${runtimeName}' runtime` : `runtime '${runtimeName}' does not exist`
+        throw new errors.ValidationError(`Validation Error: ${message}`)
+    }
+    return ingressHost
+}
 
 export const errors = {
     EventSourceError: class extends Error {
