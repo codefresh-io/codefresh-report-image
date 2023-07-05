15f3374463f45744bf4bbe4d7987dac2ee565ee6
Vadim Kharin <vadim.kharin@codefresh.io>
2023-07-05 17:48:25 +0300

    CR-18923 CF_DOCKERFILE_CONTENT payload too large for EventSource

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import { GraphQLClient, gql } from 'graphql-request'
 import { get } from 'lodash'
+import { gte } from 'semver'
 
 import { errors } from './errors'
 
@@ -12,17 +13,25 @@ export namespace Utils {
         const esc = encodeURIComponent
         const headers = { 'authorization': payload['CF_API_KEY']! }
         const runtimeName = payload['CF_RUNTIME_NAME']
+        const platformHost = payload['CF_PLATFORM_URL']
+        const runtimeVersion = await Utils.getRuntimeVersion(headers, runtimeName, platformHost)
+        const shouldStorePayloadInHeaders = gte(runtimeVersion, '0.0.553')
         let host
         if (!runtimeName) {
             host = payload['CF_HOST']
             delete payload['CF_HOST']
         } else {
-            const platformHost = payload['CF_PLATFORM_URL']
             host = await Utils.getRuntimeIngressHost(runtimeName, headers, platformHost)
             delete payload['CF_RUNTIME_NAME']
             delete payload['CF_PLATFORM_URL']
         }
         delete payload['CF_API_KEY']
+        if (shouldStorePayloadInHeaders) {
+            const qs = Object.entries(payload).map(kv => `${kv[0]}=${kv[1] || ''}`).join('&')
+            const data = Buffer.from(qs, 'binary').toString('base64')
+            headers['CF_DATA'] = data
+            return { url: `${host}/app-proxy/api/image-report`, headers }
+        }
         const qs = Object.entries(payload).map(kv => `${esc(kv[0])}=${esc(kv[1] || '')}`).join('&')
         const url = `${host}/app-proxy/api/image-report?${qs}`
         if (payload['CF_LOCAL']) {
@@ -50,6 +59,23 @@ export namespace Utils {
             throw new errors.ValidationError(message)
         }
         return ingressHost
+    }
+
+    export async function getRuntimeVersion(headers: Record<string, string>, runtimeName?: string , platformHost = 'https://g.codefresh.io'): Promise<string> {
+        if (!runtimeName) {
+            return ''
+        }
+        const graphQLClient = new GraphQLClient(`${platformHost}/2.0/api/graphql`, {
+            headers
+        })
+        const getRuntimeIngressHostQuery = gql`
+            query Runtime($name: String!) {
+                runtime(name: $name) {
+                    runtimeVersion
+                }
+            }`
+        const res = await graphQLClient.request(getRuntimeIngressHostQuery, { name: runtimeName })
+        return get(res, 'runtime.runtimeVersion', '')
     }
 
     export function tryParseJson (str: string) {
